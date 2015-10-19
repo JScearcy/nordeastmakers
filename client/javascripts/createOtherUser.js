53d6033385f6ead925b186a1e11f25cfa0cb7916
JScearcy <jacobscearcy@gmail.com>
2015-10-19 14:04:36 -0500

    Add admin user addition page

---
+++ client/javascripts/createOtherUser.js
@@ -0,0 +1,20 @@
+app.controller('createOtherUser', ['$rootScope', '$scope', '$http', '$location', function ($rootScope, $scope, $http, $location) {
+
+
+  //take first page and add to rootscope user
+  $scope.newUser = function(){
+
+      if($scope.user.password === $scope.user.verifypassword) {
+
+        $rootScope.user = $scope.user;
+        $rootScope.user.billDate = '9999-99-99';
+        $http.post('/freeusers', $rootScope.user)
+            .then(function(res) {
+                  alert('Success');
+                  $rootScope.user = {};
+            });
+        $scope.user = {};
+    }
+  };
+
+}]);
